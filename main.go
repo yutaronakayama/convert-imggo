c175eb9cdaaa5583ce343c8bce97638d99c5e1bd
Yutaro Nakayama <yutaronakayama@YutaronoMacBook-Pro.local>
2022-03-14 08:50:07 +0900

    mod main.go

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	flag.Parse()
 	fmt.Printf("受け取った引数の確認：%v\n", flag.Args())
-	converter.convert_img.convertToPNG(flag.Args(0), flag.Args(1))
+	convert_imggo.convert.convertToPNG(flag.Args(0), flag.Args(1))
+	//convert.convertToPNG()
 	fmt.Printf("Hello World Time: %v\n", time.Now())
 }
